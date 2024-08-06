6204fe96030811c1f79ceb47a23e11469f5a0ed1
Farjanamunni <farjanamunni87@gmail.com>
2024-08-06 18:15:45 -0500

    first commit

---
+++ src/louchBrowser/LaunchChrome.java
@@ -0,0 +1,15 @@
+package louchBrowser;
+
+import java.util.Scanner;
+
+public class LaunchChrome {
+//variables/attribute/field
+	//method
+	
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		//WebDriver driver = new ChromeDriver () ;
+	}
+}
